f947eeeb2aa09ed6487fa3cf9163fe42dd30a418
Sayantan Chatterjee <thisissayantan.mail@gmail.com>
2020-01-03 16:47:09 +0530

    Add GET action views

--- django-server/api/serializers.py
+++ django-server/api/serializers.py
@@ -5,5 +5,5 @@ from .models import Dataset
 class DatasetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dataset
-        fields = ['id', 'title', 'filename', 'dataframe', 'timestamp']
+        fields = ['id', 'title', 'filename', 'csv_data', 'dataframe', 'timestamp']
         read_only_fields = ['filename', 'timestamp']
--- django-server/api/views.py
+++ django-server/api/views.py
@@ -1,5 +1,7 @@
+from io import BytesIO
+import os
+from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
 from rest_framework.exceptions import ParseError
 from rest_framework.generics import (
     ListCreateAPIView,
@@ -41,13 +43,25 @@ class DatasetRetrieveDestroyAPIView(RetrieveDestroyAPIView):
             'size': dataset.dataframe.size
         })
 
-
 class DatasetRetrieveExcelAPIView(RetrieveAPIView):
     lookup_field = 'id'
     serializer_class = DatasetSerializer
 
     def get_queryset(self):
         return Dataset.objects.all()
+
+    def get(self, request, id, *args, **kwargs):
+        dataset = Dataset.objects.get(pk=id)
+        bytesio = BytesIO()
+        excel_filename = dataset.filename.replace('.csv', '.xlsx')
+        excel_writer = pd.ExcelWriter(bytesio, engine='xlsxwriter')
+        df = dataset.dataframe
+        df.to_excel(excel_writer, sheet_name='sheet1')
+        excel_writer.save()
+        bytesio.seek(0)
+        response = HttpResponse(bytesio.getvalue(), content_type='application/ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(excel_filename)
+        return response
 
 class DatasetRetrieveStatsAPIView(RetrieveAPIView):
     lookup_field = 'id'
@@ -56,9 +70,26 @@ class DatasetRetrieveStatsAPIView(RetrieveAPIView):
     def get_queryset(self):
         return Dataset.objects.all()
 
+    def get(self, request, id, *args, **kwargs):
+        dataset = Dataset.objects.get(pk=id)
+        return Response(dataset.dataframe.describe())
+
 class DatasetRetrievePlotAPIView(RetrieveAPIView):
     lookup_field = 'id'
     serializer_class = DatasetSerializer
 
     def get_queryset(self):
         return Dataset.objects.all()
+
+    def get(self, request, id, *args, **kwargs):
+        dataset = Dataset.objects.get(pk=id)
+        plot_path = os.path.join(settings.MEDIA_ROOT, 'plot.pdf')
+        df = dataset.dataframe.select_dtypes('number')
+        ax = df.plot.hist()
+        fig = ax.get_figure()
+        fig.savefig(plot_path)
+        response = {}
+        with open(plot_path, 'rb') as plot_file:
+            response = HttpResponse(plot_file, content_type='application/pdf')
+        os.remove(plot_path)
+        return response
