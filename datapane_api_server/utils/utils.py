6b24706819b41f83d48c5226b6c1bcb3a5d3267a
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-27 20:24:46 +0300

    setup utility functions for api task

--- datapane_api_server/utils/utils.py
+++ datapane_api_server/utils/utils.py
@@ -1,1 +1,31 @@
 import pandas as pd
+
+# retrieve all uploaded datasets
+def list_uploaded_datasets():
+    pass
+
+# create new dataset with uploaded csv
+def create_dataset(dataset):
+    pass
+
+# return attributes of specific dataset
+def find_dataset(id):
+    pass
+
+#delete specific dataset
+
+def delete_dataset(id):
+    pass
+
+# export dataset to excel
+def export_to_excel(id):
+    pass
+
+#return stats for dataset
+
+def dataset_stats(id):
+    pass
+
+# return histogram in pdf for specific dataset
+def generate_histogram_in_pdf(id):
+    pass
