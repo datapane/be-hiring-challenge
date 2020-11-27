b19e3891af3fc2cf653f42051c0dc1b3427bfc95
Kimaru thagana <macbuntu@localhost.localdomain>
2020-11-27 20:06:30 +0300

    setup datapane rest app and utils folder

--- datapane_api_server/datapane_api_server/settings.py
+++ datapane_api_server/datapane_api_server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- datapane_api_server/dataset_api_server/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatasetApiServerConfig(AppConfig):
-    name = 'dataset_api_server'
