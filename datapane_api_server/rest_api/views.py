85ac67f081d47ffdf5eba1dfc1d03127508fee35
Kimaru thagana <macbuntu@192.168.1.7>
2020-11-30 20:50:27 +0300

    viewsets

--- datapane_api_server/rest_api/views.py
+++ datapane_api_server/rest_api/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import reverse, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 from rest_framework.viewsets import ViewSet
+
 
 from .serializers import DatasetSerializer
 from .models import Dataset
@@ -20,6 +21,18 @@ import matplotlib.pyplot as plt
 class DatasetView(ViewSet):
 
     serializer_class = DatasetSerializer
+
+    def retrieve(self, request, pk=None):
+        queryset = Dataset.objects.all()
+        dataset = get_object_or_404(queryset, pk=pk)
+        df = pd.read_csv(dataset.dataset)
+        content = {
+            'dataset': dataset.dataset_name,
+            'file_name': dataset.filename(),
+            'size': len(df)
+        }
+        return Response(content)
+
 
     def list(self, request):
         datasets = Dataset.objects.all()
@@ -46,95 +59,66 @@ class DatasetView(ViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-
-@api_view(['GET'])
-def single_dataset(request, pk):
-    try:
-        dataset = Dataset.objects.get(pk=pk)
-
-    except ObjectDoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-
-        df = pd.read_csv(dataset.dataset)
-        content = {
-                    'dataset':dataset.dataset_name,
-                   'file_name':dataset.filename(),
-                   'size':len(df)
-                   }
-        return Response(content)
-
-
-@api_view(['DELETE', 'GET'])
-def delete_dataset(request, pk):
-    try:
-        dataset = Dataset.objects.get(pk=pk)
-
-    except ObjectDoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        dataset.dataset.delete(save=True)
-        dataset.delete()
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+        except Http404:
+            pass
         return HttpResponseRedirect(redirect_to=reverse('rest_api:datasets-list'))
 
+    @action(detail=True)
+    def describe_dataset(self, request, pk):
+        try:
+            dataset = Dataset.objects.get(pk=pk)
 
-@api_view(['GET'])
-def describe_dataset(request, pk):
-    try:
-        dataset = Dataset.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return HttpResponse(status=404)
 
-    except ObjectDoesNotExist:
-        return HttpResponse(status=404)
+        if request.method == 'GET':
+            df = pd.read_csv(dataset.dataset)
 
-    if request.method == 'GET':
-        df = pd.read_csv(dataset.dataset)
-
-        return Response(df.describe().to_json())
-
-
-@api_view(['GET'])
-def excel_dataset(request, pk):
-    try:
-        dataset = Dataset.objects.get(pk=pk)
-
-    except ObjectDoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        df = pd.read_csv(dataset.dataset)
-        file_name = f'{settings.BASE_DIR}/server_files/{dataset.dataset_name}.xlsx'
-        df.to_excel(file_name,index=False,header=True)
-
-        with open(file_name, "rb") as excel:
-            response = HttpResponse(excel.read(),content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            response['Content-Disposition']= f'inline; filename={file_name.split("/")[-1]}'
-            return response
+            return Response(df.describe().to_json())
 
 
-@api_view(['GET'])
-def plot_dataset(request, pk):
-    try:
-        dataset = Dataset.objects.get(pk=pk)
+    @action(detail=True)
+    def excel_dataset(self, request, pk):
+        try:
+            dataset = Dataset.objects.get(pk=pk)
 
-    except ObjectDoesNotExist:
-        return HttpResponse(status=404)
+        except ObjectDoesNotExist:
+            return HttpResponse(status=404)
 
-    if request.method == 'GET':
-        df = pd.read_csv(dataset.dataset)
-        numeric_column_dataset = df.select_dtypes('number')
-        file_name = f'{settings.BASE_DIR}/server_files/{dataset.dataset_name}.pdf'
-        with PdfPages(file_name) as pdf_file:
-            numeric_column_dataset.hist(bins=30, figsize=(15, 10))
-            plt.grid(True)
-            pdf_file.savefig()
-            plt.close()
+        if request.method == 'GET':
+            df = pd.read_csv(dataset.dataset)
+            file_name = f'{settings.BASE_DIR}/server_files/{dataset.dataset_name}.xlsx'
+            df.to_excel(file_name, index=False, header=True)
 
-        with open(file_name, "rb") as pdf:
+            with open(file_name, "rb") as excel:
+                response = HttpResponse(excel.read(),
+                                        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+                response['Content-Disposition'] = f'inline; filename={file_name.split("/")[-1]}'
+                return response
 
-            response = HttpResponse(pdf.read(), content_type='application/pdf')
-            response['Content-Disposition'] = f'inline; filename={file_name.split("/")[-1]}'
-            return response
+    @action(detail=True)
+    def plot_dataset(self, request, pk):
+        try:
+            dataset = Dataset.objects.get(pk=pk)
+
+        except ObjectDoesNotExist:
+            return HttpResponse(status=404)
+
+        if request.method == 'GET':
+            df = pd.read_csv(dataset.dataset)
+            numeric_column_dataset = df.select_dtypes('number')
+            file_name = f'{settings.BASE_DIR}/server_files/{dataset.dataset_name}.pdf'
+            with PdfPages(file_name) as pdf_file:
+                numeric_column_dataset.hist(bins=30, figsize=(15, 10))
+                plt.grid(True)
+                pdf_file.savefig()
+                plt.close()
+
+            with open(file_name, "rb") as pdf:
+                response = HttpResponse(pdf.read(), content_type='application/pdf')
+                response['Content-Disposition'] = f'inline; filename={file_name.split("/")[-1]}'
+                return response
